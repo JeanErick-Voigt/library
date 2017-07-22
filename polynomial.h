fc334d87f33fc5bd229bf316307b83e62f772272
Jean-Erick Voigt <voigtjeanerick@gmail.com>
2017-07-22 10:20:38 -0400

    The Header file containing struct and prototypes

---
+++ polynomial.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+//header file for polynomial.  Includes prototypes for functions.
+
+struct term {
+	int coeff;
+	unsigned int exp;
+	struct term *next;
+};
+typedef struct term polynomial;
+
+void term_create(int coeff, unsigned int exp, polynomial **eqPtr);
+void poly_print(polynomial *eqn);
+char *poly_to_string(const polynomial *p);
+bool poly_equal(const polynomial *a, const polynomial *b);
+double poly_eval(const polynomial *p, double x);
+void poly_iterate(polynomial *p, void(*transform)(struct term *p));
+void triple(struct term *p);
+
